439a5e44858d708facf4a293244f2c2b0b5ff3b3
Airek64 <emille29@gmu.edu>
2016-04-16 12:58:40 -0400

    4 asteroid sizes
    updated asteroid to have different sizes, but collision doesn't break
    asteroids properly. needs fixing.

--- js/Level1.js
+++ js/Level1.js
@@ -23,8 +23,18 @@ Game.Level1 = function (game) {
     this.ground = null;
     this.fullScreenToggle = null;
     this.asteroids = null;
-    this.asteroidTimer = null;
-    this.asteroidSpawnRate = null;
+//    this.asteroidTimer = null;
+//    this.asteroidSpawnRate = null;
+    
+    this.largeAsteroidTimer = null;
+    this.largeAsteroidSpawnRate = null;
+    this.smallAsteroidTimer = null;
+    this.smallAsteroidSpawnRate = null;
+    this.standardAsteroidTimer = null;
+    this.standardAsteroidSpawnRate = null;
+    this.tinyAsteroidTimer = null;
+    this.tinyAsteroidSpawnRate = null;
+    
     this.healthbar = null;
     this.hitTimer = null;
     this.style = null;
@@ -62,8 +72,17 @@ Game.Level1.prototype = {
         
         Game.asteroidSpawner.init();
         
-        this.asteroidTimer = 800;
-        this.asteroidSpawnRate = 150;
+//        this.asteroidTimer = 800;
+//        this.asteroidSpawnRate = 150;
+        
+        this.largeAsteroidTimer = 1000;
+        this.largeAsteroidSpawnRate = 1200;
+        this.smallAsteroidTimer = 900;
+        this.smallAsteroidSpawnRate = 800;
+        this.standardAsteroidTimer = 700;
+        this.standardAsteroidSpawnRate = 600;
+        this.tinyAsteroidTimer = 1200;
+        this.tinyAsteroidSpawnRate = 2000;
         
         this.tester = 0;
         
@@ -96,16 +115,59 @@ Game.Level1.prototype = {
         Game.asteroidSpawner.update();
         
         //this.game.physics.arcade.collide(Game.player.sprite, Game.asteroidSpawner.asteroids);
-        this.game.physics.arcade.collide(Game.asteroidSpawner.asteroids, Game.asteroidSpawner.asteroids);
-        this.game.physics.arcade.collide(Game.player.lasers, Game.asteroidSpawner.asteroids, this.laserHitAsteroid, null, this);
+        //this.game.physics.arcade.collide(Game.asteroidSpawner.asteroids, Game.asteroidSpawner.asteroids);
+        this.game.physics.arcade.collide(Game.asteroidSpawner.largeAsteroids, Game.asteroidSpawner.largeAsteroids);
+        this.game.physics.arcade.collide(Game.asteroidSpawner.largeAsteroids, Game.asteroidSpawner.standardAsteroids);
+        this.game.physics.arcade.collide(Game.asteroidSpawner.largeAsteroids, Game.asteroidSpawner.smallAsteroids);
+        this.game.physics.arcade.collide(Game.asteroidSpawner.largeAsteroids, Game.asteroidSpawner.tinyAsteroids);
+        this.game.physics.arcade.collide(Game.asteroidSpawner.standardAsteroids, Game.asteroidSpawner.standardAsteroids);
+        this.game.physics.arcade.collide(Game.asteroidSpawner.standardAsteroids, Game.asteroidSpawner.smallAsteroids);
+        this.game.physics.arcade.collide(Game.asteroidSpawner.standardAsteroids, Game.asteroidSpawner.tinyAsteroids);
+        this.game.physics.arcade.collide(Game.asteroidSpawner.smallAsteroids, Game.asteroidSpawner.smallAsteroids);
+        this.game.physics.arcade.collide(Game.asteroidSpawner.smallAsteroids, Game.asteroidSpawner.tinyAsteroids);
+        this.game.physics.arcade.collide(Game.asteroidSpawner.tinyAsteroids, Game.asteroidSpawner.tinyAsteroids);
         
-        if (this.game.time.now > this.asteroidTimer){
-            Game.asteroidSpawner.spawnAsteroid();
-            this.asteroidTimer = this.game.time.now + this.asteroidSpawnRate;
+        //this.game.physics.arcade.collide(Game.player.lasers, Game.asteroidSpawner.asteroids, this.laserHitAsteroid, null, this);
+        this.game.physics.arcade.collide(Game.player.lasers, Game.asteroidSpawner.largeAsteroids, this.laserHitLargeAsteroid, null, this);
+        this.game.physics.arcade.collide(Game.player.lasers, Game.asteroidSpawner.smallAsteroids, this.laserHitAsteroid, null, this);
+        this.game.physics.arcade.collide(Game.player.lasers, Game.asteroidSpawner.standardAsteroids, this.laserHitAsteroid, null, this);
+        this.game.physics.arcade.collide(Game.player.lasers, Game.asteroidSpawner.tinyAsteroids, this.laserHitTinyAsteroid, null, this);
+        
+//        if (this.game.time.now > this.asteroidTimer){
+//            Game.asteroidSpawner.spawnAsteroid();
+//            this.asteroidTimer = this.game.time.now + this.asteroidSpawnRate;
+//            this.tester +=1;
+//        }
+        
+        if (this.game.time.now > this.largeAsteroidTimer){
+            Game.asteroidSpawner.spawnLargeAsteroid();
+            this.largeAsteroidTimer = this.game.time.now + this.largeAsteroidSpawnRate;
             this.tester +=1;
         }
         
-        this.game.physics.arcade.overlap(Game.player.sprite, Game.asteroidSpawner.asteroids, this.playerHitAsteroid, null, this);
+        if (this.game.time.now > this.standadrdAsteroidTimer){
+            Game.asteroidSpawner.spawnStandardAsteroid();
+            this.standardAsteroidTimer = this.game.time.now + this.standardAsteroidSpawnRate;
+            this.tester +=1;
+        }
+        
+        if (this.game.time.now > this.smallAsteroidTimer){
+            Game.asteroidSpawner.spawnSmallAsteroid();
+            this.smallAsteroidTimer = this.game.time.now + this.smallAsteroidSpawnRate;
+            this.tester +=1;
+        }
+        
+        if (this.game.time.now > this.tinyAsteroidTimer){
+            Game.asteroidSpawner.spawnTinyAsteroid();
+            this.tinyAsteroidTimer = this.game.time.now + this.tinyAsteroidSpawnRate;
+            this.tester +=1;
+        }
+        
+        //this.game.physics.arcade.overlap(Game.player.sprite, Game.asteroidSpawner.asteroids, this.playerHitAsteroid, null, this);
+        this.game.physics.arcade.overlap(Game.player.sprite, Game.asteroidSpawner.largeAsteroids, this.playerHitLargeAsteroid, null, this);
+        this.game.physics.arcade.overlap(Game.player.sprite, Game.asteroidSpawner.standardAsteroids, this.playerHitAsteroid, null, this);
+        this.game.physics.arcade.overlap(Game.player.sprite, Game.asteroidSpawner.smallAsteroids, this.playerHitAsteroid, null, this);
+//        this.game.physics.arcade.overlap(Game.player.sprite, Game.asteroidSpawner.tinyAsteroids, this.playerHitAsteroid, null, this);
         
         this.healthbar.width = Game.player.health * 5;
         
@@ -115,11 +177,19 @@ Game.Level1.prototype = {
             Game.level++;
             this.state.start('LevelComplete');
         }
-        else if (Game.distance >= 10000 * Game.level - 600) {
-            this.asteroidSpawnRate = 100000;
+        else if (Game.distance >= 10000 * Game.level - 800) {
+            //this.asteroidSpawnRate = 10000000;
+            this.largeAsteroidSpawnRate = 10000000;
+            this.standardAsteroidSpawnRate = 10000000;
+            this.smallAsteroidSpawnRate = 10000000;
+            this.tinyAsteroidSpawnRate = 10000000;
         }
-        else if (Game.distance >= 6000 * (Game.level - 1) + 250) {
-            this.asteroidSpawnRate = 250 / Game.level;
+        else if (Game.distance >= 6000 * (Game.level - 1) + 1000) {
+            //this.asteroidSpawnRate = 250 / Game.level;
+            this.largeAsteroidSpawnRate = 1200 / Game.level;
+            this.standardAsteroidSpawnRate = 800 / Game.level;
+            this.smallAsteroidSpawnRate = 600 / Game.level;
+            this.tinyAsteroidSpawnRate = 2000 / Game.level;
         }
         
         if (Game.player.health <= 0) {
@@ -151,7 +221,60 @@ Game.Level1.prototype = {
         
     },
     
+    playerHitLargeAsteroid: function (player, asteroid) {
+        if (Game.asteroidSpawner.speed <= -250 && Math.abs(player.y - asteroid.y) < 10) {
+            Game.player.health -= 50;
+            Game.player.damage();
+            this.hitTimer = this.game.time.now + 250;
+            
+        }
+        
+        else if (this.game.time.now > this.hitTimer) {
+            Game.player.health -= asteroid.scale.x * 2.5 + (-asteroid.body.velocity.x / 100) * 1.5;
+            Game.player.damage();
+            this.hitTimer = this.game.time.now + 100;
+        }
+        if (asteroid.y > player.y) {
+            player.y -= 3;
+            asteroid.y += 1;
+            asteroid.body.angularVelocity = 50;
+        }
+        else if (asteroid.y < player.y) {
+            player.y += 3;
+            asteroid.y -= 1;
+            asteroid.body.angularVelocity = -50;
+        }
+        
+    },
+    
     laserHitAsteroid: function (laser, asteroid) {
+        asteroid.kill();
+        laser.kill();
+    },
+    
+    laserHitLargeAsteroid: function (laser, asteroid) {
+        
+        laser.kill();
+        
+//        var newAsteroid = Game.asteroidSpawner.smallAsteroids.getFirstExists(false);
+//        
+//        if (newAsteroid) {
+//            newAsteroid.reset(asteroid.x + (Math.random() + (5 - 2) + 2), asteroid.y + (Math.random() + (5 - 2) + 2));
+//            newAsteroid.body.velocity.y = Math.random() * 40 - 20;
+//        }
+//        
+//        asteroid = Game.asteroidSpawner.smallAsteroids.getFirstExists(false);
+//        
+//        if (newAsteroid) {
+//            newAsteroid.reset(asteroid.x + (Math.random() + (10 - 2) + 2), asteroid.y - (Math.random() + (10 - 2) + 2));
+//            newAsteroid.body.velocity.y = Math.random() * 40 - 20;
+//        }
+        
+        asteroid.kill();
+        
+    },
+    
+    laserHitTinyAsteroid: function (laser, asteroid) {
         asteroid.kill();
         laser.kill();
     },
